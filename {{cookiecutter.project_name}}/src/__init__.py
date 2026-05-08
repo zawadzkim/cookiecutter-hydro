26545e076df1539640283aaba1a376d8f8419676
zawadzkim <zawadzkimat@outlook.com>
2026-05-08 14:21:44 +0200

    chore: remove src/foo.py stub and its test

--- {{cookiecutter.project_name}}/src/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Source modules for modeling workflows."""
-
-from .foo import foo
-
-__all__ = ["foo"]
--- {{cookiecutter.project_name}}/tests/test_foo.py
+++
@@ -1,6 +0,0 @@
-from src.foo import foo
-
-
-def test_foo():
-    """Example pytest: import a function and assert on expected behavior."""
-    assert foo("foo") == "foo"
